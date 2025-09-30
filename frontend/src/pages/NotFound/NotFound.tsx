8f55343f64d6ed524d50704329fce8c1b098e965
koriahin-serhii <koriahin.s.work@gmail.com>
2025-09-30 19:36:07 +0200

    created page not found

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,26 @@
+import styles from './NotFound.module.css';
+import loginImage from '@assets/images/login.png';
+
 export default function NotFound() {
-  return <div>404 Not Found</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.imageContainer}>
+          <img
+            src={loginImage}
+            alt="Phone with Instagram app"
+            className={styles.phoneImage}
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Oops! Page Not Found (404 Error)</h1>
+          <p className={styles.description}>
+            We're sorry, but the page you're looking for doesn't seem to exist.
+            If you typed the URL manually, please double-check the spelling. If
+            you clicked on a link, it may be outdated or broken.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }

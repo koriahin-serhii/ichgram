bc11f1b4a11abdf7bf261f5fd45a0ed7a63e90cb
koriahin-serhii <koriahin.s.work@gmail.com>
2025-10-08 00:04:26 +0200

    structure adjustment

--- frontend/src/shared/components/index.ts
+++ frontend/src/shared/components/index.ts
@@ -1,7 +1,7 @@
 export { default as UserAvatar } from './UserAvatar/UserAvatar';
 export { default as PostGrid } from './PostGrid/PostGrid';
 export { default as ProfileHeader } from './ProfileHeader/ProfileHeader';
-export { default as SearchSidebar } from './SearchSidebar/SearchSidebar';
-export { default as NotificationsSidebar } from './NotificationsSidebar/NotificationsSidebar';
+export { default as SearchSidebar } from '../../components/SearchSidebar/SearchSidebar';
+export { default as NotificationsSidebar } from '../../components/NotificationsSidebar/NotificationsSidebar';
 export { default as UserSearchResult } from './UserSearchResult/UserSearchResult';
 export { default as CreatePostModal } from './CreatePostModal/CreatePostModal';
